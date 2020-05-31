11c49dccf68d82da246f697870bf38bbfffdb258
khaoula ab <khaoulabichara@gmail.com>
2020-05-31 20:14:47 +0200

    commit 8

--- Elearning/src/main/java/com/example/demo/controllers/ChatController.java
+++ Elearning/src/main/java/com/example/demo/controllers/ChatController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controllers;
 
 
+import com.example.demo.entities.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 import com.example.demo.entities.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,4 +43,18 @@ public class ChatController {
 
         return "chat";
     }
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
 }
--- Elearning/src/main/resources/static/js/chat.js
+++ Elearning/src/main/resources/static/js/chat.js
@@ -1,81 +1,121 @@
-const url = 'http://localhost:3030';
-let stompClient;
-let selectedUser;
-let newMessages = new Map();
+'use strict';
 
-function connectToChat(userName) {
-    console.log("connecting to chat...")
-    let socket = new SockJS(url + '/chat');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        console.log("connected to: " + frame);
-        stompClient.subscribe("/topic/messages/" + userName, function (response) {
-            let data = JSON.parse(response.body);
-            if(selectedUser==data.fronLogin){
-                render(data.message,data.fronLogin);
-            }else{
-                newMessages.set(data.fronLogin,data.message);
-                $('#userNameAppender_'+data.fronLogin).append('<span id="newMessage_' + data.fronLogin + '"  style="color:red">+1</span>')
-            }
-        });
-    });
-}
-function  sendMsg(fron,text) {
-    stompClient.send("/app/chat/"+ selectedUser,{},JSON.stringify({
-        fronLogin:fron,
-        message:text
-    }));
+var usernamePage = document.querySelector('#username-page');
+var chatPage = document.querySelector('#chat-page');
+var usernameForm = document.querySelector('#usernameForm');
+var messageForm = document.querySelector('#messageForm');
+var messageInput = document.querySelector('#message');
+var messageArea = document.querySelector('#messageArea');
+var connectingElement = document.querySelector('.connecting');
 
-}
+var stompClient = null;
+var username = null;
 
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
 
-function registration() {
-   let userName=document.getElementById('userName').value;
-    $.get(url+"/registration/"+ userName,function (response){
-            connectToChat(userName);
-        }).fail(function (error) {
-            if(error.status===400){
-                alert("Login is already busy!")
+function connect(event) {
+    username = document.querySelector('#name').value.trim();
 
-            }
+    if(username) {
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
 
-    })
-}
+        var socket = new SockJS('/javatechie');
+        stompClient = Stomp.over(socket);
 
-
-function selectUser(userName){
-    console.log("selecting users: "+userName);
-    selectedUser=userName;
-    let isNew = document.getElementById("newMessage_"+userName)!==null;
-    if(isNew){
-        let element= document.getElementById("newMessage_" + userName);
-        element.parentNode.removeChild(element);
-        render(newMessages.get(userName),userName)
+        stompClient.connect({}, onConnected, onError);
     }
-    $('#selectedUserId').html('');
-    $('#selectedUserId').append('Chat avec '+userName);
+    event.preventDefault();
 }
 
 
+function onConnected() {
+    // Subscribe to the Public Topic
+    stompClient.subscribe('/topic/public', onMessageReceived);
 
+    // Tell your username to the server
+    stompClient.send("/app/chat.register",
+        {},
+        JSON.stringify({sender: username, type: 'JOIN'})
+    )
 
-
-function fetchAll() {
-    $.get(url + "/fetchAllUsers", function (response) {
-        let users = response;
-        let usersTemplateHTML = "";
-        for (let i = 0; i < users.length; i++) {
-            usersTemplateHTML = usersTemplateHTML + '<a href="#" onclick="selectUser(\'' + users[i] + '\')"><li class="clearfix">\n' +
-                '                <img src="https://rtfm.co.ua/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" width="55px" height="55px" alt="avatar" />\n' +
-                '                <div class="about">\n' +
-                '                    <div id="userNameAppender_' + users[i] + '" class="name">' + users[i] + '</div>\n' +
-                '                    <div class="status">\n' +
-                '                        <i class="fa fa-circle offline"></i>\n' +
-                '                    </div>\n' +
-                '                </div>\n' +
-                '            </li></a>';
-        }
-        $('#usersList').html(usersTemplateHTML);
-    });
+    connectingElement.classList.add('hidden');
 }
 
+
+function onError(error) {
+    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    connectingElement.style.color = 'red';
+}
+
+
+function send(event) {
+    var messageContent = messageInput.value.trim();
+
+    if(messageContent && stompClient) {
+        var chatMessage = {
+            sender: username,
+            content: messageInput.value,
+            type: 'CHAT'
+        };
+
+        stompClient.send("/app/chat.send", {}, JSON.stringify(chatMessage));
+        messageInput.value = '';
+    }
+    event.preventDefault();
+}
+
+
+function onMessageReceived(payload) {
+    var message = JSON.parse(payload.body);
+
+    var messageElement = document.createElement('li');
+
+    if(message.type === 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' a rejoint!';
+    } else if (message.type === 'LEAVE') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' est parti!';
+    } else {
+        messageElement.classList.add('chat-message');
+
+        var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
+        var usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+
+function getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
+}
+
+usernameForm.addEventListener('submit', connect, true)
+messageForm.addEventListener('submit', send, true)
